81ebb23bbf6e7c4bce11052c71e41b87e002e578
ArmanLots <135289283+ArmanLots@users.noreply.github.com>
2023-06-02 21:26:01 -0300

    Wep

---
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_writherzombie.lua
@@ -0,0 +1,51 @@
+AddCSLuaFile()
+
+SWEP.PrintName = "Writher Zombie"
+
+SWEP.Base = "weapon_zs_zombie"
+
+SWEP.MeleeDamage = 37
+SWEP.MeleeForceScale = 1.25
+
+SWEP.Primary.Delay = 1.4
+
+function SWEP:Reload()
+	self.BaseClass.SecondaryAttack(self)
+end
+
+function SWEP:PlayAlertSound()
+	self:PlayAttackSound()
+end
+
+function SWEP:PlayIdleSound()
+	self:GetOwner():EmitSound("npc/barnacle/barnacle_tongue_pull"..math.random(3)..".wav")
+end
+
+function SWEP:PlayAttackSound()
+	self:EmitSound("npc/ichthyosaur/attack_growl"..math.random(3)..".wav", 70, math.Rand(100, 110))
+end
+
+if not CLIENT then return end
+
+function SWEP:ViewModelDrawn()
+	render.ModelMaterialOverride(0)
+end
+
+local matSheet = Material("models/weapons/v_zombiearms/ghoulsheet")
+function SWEP:PreDrawViewModel(vm)
+	render.ModelMaterialOverride(matSheet)
+	render.SetColorModulation(0, 0, 0)
+end
+
+function SWEP:ApplyMeleeDamage(ent, trace, damage)
+	if ent:IsPlayer() then
+		ent:GiveStatus("dimvision", 6)
+		local owner = self:GetOwner()
+
+		if gt and gt:IsValid() then
+			gt.Applier = owner
+		end
+	end
+
+	self.BaseClass.ApplyMeleeDamage(self, ent, trace, damage)
+end
