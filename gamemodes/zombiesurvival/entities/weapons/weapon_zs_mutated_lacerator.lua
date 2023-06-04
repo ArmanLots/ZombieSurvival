10bfe2fa2fd30e34434306b1faef034c5ed4a90f
ArmanLots <135289283+ArmanLots@users.noreply.github.com>
2023-06-04 19:47:39 +0000

    Fix

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_mutated_lacerator.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_mutated_lacerator.lua
@@ -12,8 +12,18 @@ SWEP.PounceDamage = 32
 
 function SWEP:MeleeHit(ent, trace, damage, forcescale)
 	if not ent:IsPlayer() then
-		damage = math.floor(damage * 18/22)
+		damage = math.floor(damage * 1.2)
 	end
 
 	ent:PoisonDamage(damage, self:GetOwner(), self, trace.HitPos)
+end
+
+if not CLIENT then return end
+
+function SWEP:ViewModelDrawn()
+	render.ModelMaterialOverride(0)
+end
+
+function SWEP:PreDrawViewModel(vm)
+	render.SetColorModulation(0.2, 0.5, 0)
 end
