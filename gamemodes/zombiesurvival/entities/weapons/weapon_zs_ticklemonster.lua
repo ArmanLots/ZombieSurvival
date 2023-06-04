2ad6d932de34f9d863b8d8d83a1fa3ac9ee08b00
ArmanLots <135289283+ArmanLots@users.noreply.github.com>
2023-06-04 19:59:49 +0000

    Update Model And nerf

--- gamemodes/zombiesurvival/entities/weapons/weapon_zs_ticklemonster.lua
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_ticklemonster.lua
@@ -4,10 +4,17 @@ SWEP.PrintName = "The Tickle Monster"
 
 SWEP.Base = "weapon_zs_zombie"
 
-SWEP.MeleeDamage = 24
-SWEP.MeleeDamageVsProps = 28
-SWEP.MeleeReach = 150
-SWEP.MeleeSize = 5
+SWEP.ViewModel = Model("models/weapons/v_fza.mdl")
+SWEP.WorldModel = ""
+
+if CLIENT then
+	SWEP.ViewModelFOV = 115
+end
+
+SWEP.MeleeDamage = 22
+SWEP.MeleeDamageVsProps = 24
+SWEP.MeleeReach = 115
+SWEP.MeleeSize = 2
 
 function SWEP:Reload()
 	self:SecondaryAttack()
@@ -28,15 +35,4 @@ SWEP.PlayIdleSound = SWEP.PlayAlertSound
 
 function SWEP:PlayAttackSound()
 	self:EmitSound("npc/barnacle/barnacle_bark"..math.random(2)..".wav")
-end
-
-if not CLIENT then return end
-
-function SWEP:ViewModelDrawn()
-	render.ModelMaterialOverride(0)
-end
-
-local matSheet = Material("Models/Charple/Charple1_sheet")
-function SWEP:PreDrawViewModel(vm)
-	render.ModelMaterialOverride(matSheet)
 end
