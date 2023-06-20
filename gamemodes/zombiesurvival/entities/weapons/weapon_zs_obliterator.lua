2e02353a2c55f77a2a6b45c71d5424854fc5c6f6
ArmanLots <135289283+ArmanLots@users.noreply.github.com>
2023-06-20 20:05:57 +0000

    Obliterator test

---
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_obliterator.lua
@@ -0,0 +1,45 @@
+AddCSLuaFile()
+
+if CLIENT then
+	SWEP.PrintName = "Obliterator"
+
+SWEP.Base = "weapon_zs_zombie"
+
+SWEP.MeleeDamage = 48
+SWEP.MeleeDamageVsProps = 85
+SWEP.Primary.Delay = 1.1
+
+SWEP.AlertDelay = 3.5
+
+function SWEP:Reload()
+	self:SecondaryAttack()
+end
+
+function SWEP:MeleeHit(ent, trace, damage, forcescale)
+	if not ent:IsPlayer() then
+		damage = self.MeleeDamageVsProps
+	end
+
+	self.BaseClass.MeleeHit(self, ent, trace, damage, forcescale)
+end
+
+function SWEP:PlayAlertSound()
+	self:GetOwner():EmitSound("npc/combine_gunship/gunship_moan.wav", 75, math.random(65,75))
+end
+SWEP.PlayIdleSound = SWEP.PlayAlertSound
+
+function SWEP:PlayAttackSound()
+	self:EmitSound("npc/zombie_poison/pz_warn"..math.random(2)..".wav", 80, math.random(55, 65), 0.5, CHAN_AUTO)
+	--self:EmitSound("npc/antlion_guard/angry"..math.random(3)..".wav", 80, math.random(112, 115), 0.5, CHAN_AUTO)
+end
+
+
+if not CLIENT then return end
+
+function SWEP:ViewModelDrawn()
+	render.ModelMaterialOverride(0)
+end
+
+function SWEP:PreDrawViewModel(vm)
+	render.SetColorModulation(0, 1, 0)
+end
---
+++ gamemodes/zombiesurvival/gamemode/zombieclasses/superboss_obliterator.lua
@@ -0,0 +1,137 @@
+CLASS.Name = "Obliterator"
+CLASS.TranslationName = "class_obliterator"
+CLASS.Description = "description_obliterator"
+CLASS.Help = "controls_obliterator"
+
+CLASS.SuperBoss = true
+
+CLASS.KnockbackScale = 0
+
+CLASS.FearPerInstance = 1
+
+CLASS.CanTaunt = true
+
+CLASS.Points = 140
+
+CLASS.SWEP = "weapon_zs_obliterator"
+
+CLASS.Model = Model("models/player/zombie_classic_hbfix.mdl")
+CLASS.OverrideModel = Model("models/player/zombie_lacerator2.mdl")
+
+CLASS.Health = 8750
+CLASS.DynamicHealth = 0
+CLASS.Speed = 190
+
+CLASS.VoicePitch = 0.65
+
+CLASS.ModelScale = 1
+--CLASS.Hull = {Vector(-16, -16, 0), Vector(16, 16, 72)}
+--CLASS.HullDuck = {Vector(-16, -16, 0), Vector(16, 16, 36)}
+--CLASS.ViewOffset = DEFAULT_VIEW_OFFSET * CLASS.ModelScale
+--CLASS.ViewOffsetDucked = DEFAULT_VIEW_OFFSET_DUCKED * CLASS.ModelScale
+--CLASS.StepSize = 25
+--CLASS.Mass = DEFAULT_MASS * CLASS.ModelScale
+
+--CLASS.BloodColor = BLOOD_COLOR_YELLOW
+
+local math_random = math.random
+local math_min = math.min
+local math_ceil = math.ceil
+local CurTime = CurTime
+
+local DIR_BACK = DIR_BACK
+local ACT_INVALID = ACT_INVALID
+local ACT_HL2MP_SWIM_PISTOL = ACT_HL2MP_SWIM_PISTOL
+local ACT_HL2MP_IDLE_CROUCH_ZOMBIE = ACT_HL2MP_IDLE_CROUCH_ZOMBIE
+local ACT_HL2MP_WALK_CROUCH_ZOMBIE_01 = ACT_HL2MP_WALK_CROUCH_ZOMBIE_01
+local ACT_HL2MP_RUN_ZOMBIE = ACT_HL2MP_RUN_ZOMBIE
+local ACT_GMOD_GESTURE_TAUNT_ZOMBIE = ACT_GMOD_GESTURE_TAUNT_ZOMBIE
+
+function CLASS:IgnoreLegDamage(pl, dmginfo)
+	return true
+end
+
+function CLASS:PlayerStepSoundTime(pl, iType, bWalking)
+	return GAMEMODE.BaseClass.PlayerStepSoundTime(GAMEMODE.BaseClass, pl, iType, bWalking) * 1.8
+end
+
+function CLASS:PlayDeathSound(pl)
+	pl:EmitSound("npc/ichthyosaur/water_growl5.wav", 72, 40)
+
+	return true
+end
+
+function CLASS:PlayPainSound(pl)
+	pl:EmitSound("npc/combine_soldier/pain"..math_random(3)..".wav", 75, math.Rand(60, 65))
+	pl.NextPainSound = CurTime() + 0.5
+
+	return true
+end
+
+local StepSounds = {
+	"npc/zombie_poison/pz_left_foot1.wav"
+}
+local ScuffSounds = {
+	"npc/zombie_poison/pz_right_foot1.wav"
+}
+function CLASS:PlayerFootstep(pl, vFootPos, iFoot, strSoundName, fVolume, pFilter)
+	if iFoot == 0 and math_random() < 0.333 then
+		pl:EmitSound(ScuffSounds[math_random(#ScuffSounds)], 80, 90)
+	else
+		pl:EmitSound(StepSounds[math_random(#StepSounds)], 80, 90)
+	end
+
+	return true
+end
+
+function CLASS:CalcMainActivity(pl, velocity)
+	if pl:WaterLevel() >= 3 then
+		return ACT_HL2MP_SWIM_PISTOL, -1
+	elseif pl:Crouching() then
+		if velocity:Length2DSqr() <= 1 then
+			return ACT_HL2MP_IDLE_CROUCH_ZOMBIE, -1
+		else
+			return ACT_HL2MP_WALK_CROUCH_ZOMBIE_01 - 1 + math_ceil((CurTime() / 4 + pl:EntIndex()) % 3), -1
+		end
+	else
+		return ACT_HL2MP_RUN_ZOMBIE, -1
+	end
+
+	return true
+end
+
+function CLASS:UpdateAnimation(pl, velocity, maxseqgroundspeed)
+	local len2d = velocity:Length2D()
+	if len2d > 1 then
+		pl:SetPlaybackRate(math_min(len2d / maxseqgroundspeed * 0.5 , 3))
+	else
+		pl:SetPlaybackRate(1 / self.ModelScale)
+	end
+
+	return true
+end
+
+function CLASS:DoAnimationEvent(pl, event, data)
+	if event == PLAYERANIMEVENT_ATTACK_PRIMARY then
+		pl:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_GMOD_GESTURE_RANGE_ZOMBIE, true)
+		return ACT_INVALID
+	elseif event == PLAYERANIMEVENT_RELOAD then
+		pl:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_GMOD_GESTURE_TAUNT_ZOMBIE, true)
+		return ACT_INVALID
+	end
+end
+
+if not CLIENT then return end
+
+CLASS.Icon = "zombiesurvival/killicons/howler"
+CLASS.IconColor = Color(30, 30, 30)
+
+function CLASS:PrePlayerDraw(pl)
+	render.ModelMaterialOverride(matSkin)
+	render.SetColorModulation(0.1, 0.1, 0.1)
+end
+
+function CLASS:PostPlayerDraw(pl)
+	render.ModelMaterialOverride()
+	render.SetColorModulation(1, 1, 1)
+end
